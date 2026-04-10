3b27eac0cc9b9182980b9b70834f3b1fd9bc4bd3
Shan John <shanjohn5308@gmail.com>
2026-04-11 02:54:12 +0530

    feat: initialize backend Express server and configure frontend environment variables

--- alora-backend/src/index.js
+++ alora-backend/src/index.js
@@ -22,6 +22,13 @@ app.use(cors({
   ],
   credentials: true,
 }));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
